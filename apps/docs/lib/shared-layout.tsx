ef1944dfdb8c03ba22877eb7fdc5e4224472ebaf
Edoardo Ranghieri <edoardo@ranghieri.com>
2026-03-03 12:09:59 +0100

    add logo in website topbar

--- apps/docs/lib/shared-layout.tsx
+++ apps/docs/lib/shared-layout.tsx
@@ -1,9 +1,16 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import Image from "next/image";
 
 export const baseLayoutOptions: BaseLayoutProps = {
 	githubUrl: "https://github.com/TheEdoRan/next-safe-action",
 	nav: {
-		title: "next-safe-action",
+		title: (
+			<>
+				<Image src="/img/logo-light-mode.svg" alt="" width={26} height={20} className="dark:hidden" />
+				<Image src="/img/logo-dark-mode.svg" alt="" width={26} height={20} className="hidden dark:block" />
+				next-safe-action
+			</>
+		),
 		url: "/",
 	},
 };
